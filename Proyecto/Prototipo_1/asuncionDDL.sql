97702c29bb248796768b3ff4e44ad3de7d09a142
Royer <A01209400@itesm.mx>
2020-10-10 19:12:16 +0000

    update: ddl

--- Proyecto/Prototipo_1/asuncionDDL.sql
+++ Proyecto/Prototipo_1/asuncionDDL.sql
@@ -6,7 +6,18 @@ create table Donativos
 	Telefono varchar(50) not null,
 	Correo varchar(50) not null,
 	Descripcion varchar(50) not null,
-	Fecha varchar(50) not null
+	Fecha varchar(50) not null,
+	NumeroEmpleado int
+);
+
+create table Empleados
+(
+	NumeroEmpleado varchar(50) not null,
+	NombreEmpleado varchar(50) not null,
+	Telefono varchar(50) not null,
+	Domicilio varchar(50) not null,
+	Especialidad varchar(50) not null,
+	Correo varchar(50) not null
 );
 
 create table Pacientes
@@ -27,17 +38,14 @@ create table Pacientes
 	Dependencia varchar(50),
 	Correo varchar(50),
 	Municipio varchar(50),
-	Estado varchar(50)
-);
-
-create table Terapeutas
-(
-	NumeroEmpleado varchar(50) not null,
-	NombreEmpleado varchar(50) not null,
-	Telefono varchar(50) not null,
-	Domicilio varchar(50) not null,
-	Especialidad varchar(50) not null,
-	Correo varchar(50) not null
+	Estado varchar(50),
+	Imagen varchar(200),
+	Curp varchar(200),
+	ActaNacimiento varchar(200),
+	ComprobanteDomicilio varchar(200),
+	Total int,
+	Faltas int,
+	Justificacion varchar(200)
 );
 
 create table HistorialClinico
@@ -71,6 +79,29 @@ create table municipio
 	nombreMunicipio varchar(400) not null,
 	idCab int not null,
 	nombreCab varchar(400) not null
-)
+);
 
+create table privilegios
+(
+	Id_Privilegio varchar(30) not null,
+	Accion text not null
+);
+
+create table roles
+(
+	Id_Rol varchar(30) not null,
+	Descripcion_Rol text not null
+);
+
+create table roles_Empleados
+(
+	Id_Rol varchar(30) not null,
+	NumeroEmpleado int not null
+);
+
+create table roles_privilegios
+(
+	Id_Privilegio varchar(30) not null,
+	Id_Rol varchar(30) not null
+);
 
