5ac8fd0756e5c92f8048d3bb4c0fb5e470505735
Royer <A01209400@itesm.mx>
2020-10-12 03:44:35 +0000

    fix: responsividad de dashboard

--- Proyecto/Prototipo_1/signin/session_util.php
+++ Proyecto/Prototipo_1/signin/session_util.php
@@ -47,4 +47,28 @@
         }
     }
 
+    function getRol($NumeroEmpleado){
+        $connection=connectDB();
+        $sql="SELECT Id_Rol FROM roles_Empleados WHERE NumeroEmpleado='".$NumeroEmpleado."'";
+        $queryResult=mysqli_query($connection,$sql);
+        disconectDB($connection);
+        if(mysqli_num_rows($queryResult)>0){
+            $row=mysqli_fetch_assoc($queryResult);
+            return $row["Id_Rol"];
+        }
+    }
+
+    function getPermisos($rol){
+        $permisos=array();
+        $connection=connectDB();
+        $sql="SELECT Id_Privilegio FROM roles_privilegios WHERE Id_Rol='".$rol."'";
+        $queryResult=mysqli_query($connection,$sql);
+        disconectDB($connection);
+        if(mysqli_num_rows($queryResult)>0){
+            while($row=mysqli_fetch_assoc($queryResult)){
+                array_push($permisos,$row["Id_Privilegio"]);
+            }
+            return $permisos;
+        }
+    }
 ?>
--- Proyecto/Prototipo_1/signin/signinController.php
+++ Proyecto/Prototipo_1/signin/signinController.php
@@ -9,8 +9,11 @@
     include_once("session_util.php");
     $usuario=isUser($_SESSION["usuario"]);
     $password=isPassword($usuario,$_SESSION["password"]);
+    $rol=getRol($usuario);
+    $permisos=getPermisos($rol);
+    var_dump($permisos);
+    
     if($usuario&&$password){
-        header('Location: https://royerdac99.codes/WebImpact/Proyecto/Prototipo_1/Administracion.html');
+        header('Location: https://royerdac99.codes/WebImpact/Proyecto/Prototipo_1/dasboard/dashboard.php');
     }
-
 ?>
