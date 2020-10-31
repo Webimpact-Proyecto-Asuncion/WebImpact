e5ace4f994d0aa9cf6289d3730d4a2421c5fee2d
Royer <A01209400@itesm.mx>
2020-10-31 20:47:11 +0000

    add css

--- Proyecto/Prototipo/signin/signin.php
+++ Proyecto/Prototipo/signin/signin.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include_once("../partials/_head.html");
+    include_once("./_head.html");
     //include_once("./_navbar1.html");
     include_once("./_signinform.html");
     include_once("../partials/_footer.html");
