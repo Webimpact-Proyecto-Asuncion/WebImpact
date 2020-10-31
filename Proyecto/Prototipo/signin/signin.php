aafb0246da46485a4020bb9f4b580f3ebc96661a
Royer <A01209400@itesm.mx>
2020-10-31 19:31:14 +0000

    esqueleto

--- Proyecto/Prototipo/signin/signin.php
+++ Proyecto/Prototipo/signin/signin.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    include_once("./_head.html");
+    include_once("../partials/_head.html");
     //include_once("./_navbar1.html");
     include_once("./_signinform.html");
-    include_once("./_footer.html");
+    include_once("../partials/_footer.html");
 ?>
