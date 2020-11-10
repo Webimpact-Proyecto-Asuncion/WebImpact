05107dfe77dc0f36ca6e98c94a8404831ae368e4
Royer <A01209400@itesm.mx>
2020-11-10 19:38:43 +0000

    fix: sammy

--- Proyecto/Prototipo/ConsultaPaciente/php/conexion.php
+++ Proyecto/Prototipo/ConsultaPaciente/php/conexion.php
@@ -1,10 +1,10 @@
 <?php
 
 	function conexion(){
-			$servidor="localhost";
-			$usuario="root";
-			$password="";
-			$bd="asuncion";
+		$servername="localhost";
+        $username="WITEAM";
+        $password="WebImpact$2020";
+        $dbname="asuncion1";
 
 			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
 
