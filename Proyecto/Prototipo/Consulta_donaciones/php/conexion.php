e432d9dfa08eee5c4c0ecab3ef637f0eb18d96f1
SamOctGonzAzpeitia <52299598+SamOctGonzAzpeitia@users.noreply.github.com>
2020-11-17 21:18:49 -0600

    actualizacion de conexiones en consulta

--- Proyecto/Prototipo/Consulta_donaciones/php/conexion.php
+++ Proyecto/Prototipo/Consulta_donaciones/php/conexion.php
@@ -1,9 +1,10 @@
 <?php
 
 	function conexion(){
-			$servername="localhost";
-        $username="WITEAM";
-        $password="WebImpact$2020";
+		
+		$servername="localhost";
+        $username="Matu";
+        $password="Matu$2020";
         $dbname="asuncion1";
 
 		$conexion=mysqli_connect($servername,$username,$password,$dbname);
