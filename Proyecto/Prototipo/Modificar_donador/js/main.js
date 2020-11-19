c56a5cd10cf7294c87b82fd12f71bc1798048e9d
SamOctGonzAzpeitia <52299598+SamOctGonzAzpeitia@users.noreply.github.com>
2020-11-18 21:04:08 -0600

    Se modifico consulta_historial

---
+++ Proyecto/Prototipo/Modificar_donador/js/main.js
@@ -0,0 +1,48 @@
+
+$(buscar_datos());
+
+function buscar_datos(consulta){
+    $.ajax({
+        url: 'php/tabla.php' ,
+        type: 'POST' ,
+        dataType: 'html',
+        data: {consulta: consulta},
+    })
+    .done(function(respuesta){
+        $("#tabla").html(respuesta);
+    })
+    .fail(function(){
+        console.log("error");
+    });
+}
+$(document).on('keyup','#busqueda', function(){
+    var valor = $(this).val();
+    if (valor != "") {
+        buscar_datos(valor);
+    }else{
+        buscar_datos();
+    }
+});
+
+
+
+function preguntarSiNo(id){
+
+
+
+	alertify.confirm('Eliminar Datos', '¿Esta seguro de eliminar este registro?', 
+					function(){ eliminarDatos(id) }
+                , function(){ alertify.error('Se cancelo')});
+	
+}
+
+function eliminarDatos(id){
+
+	
+ $.post("php/EliminarPacientes.php",{id:id}).
+    done(function( data ) {
+    	
+        alertify.success("Eliminado con exito!");
+        $('#tabla').load('php/tabla.php');
+    });
+}
