4a88e8b9160bbebfeaf98e5361f9391c12d6f480
Franciscodzv <franciscodzv@gmail.com>
2020-11-18 18:30:59 -0600

    Fixing donacion

---
+++ Proyecto/Prototipo/Registra_Donacion/ajaxDonacion.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+});
+
+//*Donador:
+
+let submit = document.getElementById("submitDonacion"); //validar
+let donador = document.getElementById("donador");
+let donadorC = document.getElementById("donadorCorrecto");
+let donadorE = document.getElementById("donadorError");
+
+let descripcion = document.getElementById("descripcionDonacion");
+let spanDescripcionE = document.getElementById("descripcionError");
+let spanDescripcionC = document.getElementById("descripcionCorrecta");
+
+let fecha = document.getElementById("fechaDonacion");
+let spanFechaC = document.getElementById("fechaC");
+let spanFechaE = document.getElementById("fechaE");
+
+
+$("")
