601e796a902f77ac844ae661475e10b89fd2eeab
AlbertoMatuteB <a01704584@itesm.mx>
2020-11-24 20:51:35 -0600

    fix de rol administrador

--- Proyecto/Prototipo/sidebar/index.php
+++ Proyecto/Prototipo/sidebar/index.php
@@ -2,7 +2,7 @@
   if( $_SESSION["rol"] == 'Terapeuta'){
     include ('../sidebar/_sidebarT.html');
   }
-  else if( $_SESSION["rol"] == 'Administrador'){
+  else if( $_SESSION["rol"] == 'Administracion'){
     include ('../sidebar/_sidebarA.html');
   }
 ?>
