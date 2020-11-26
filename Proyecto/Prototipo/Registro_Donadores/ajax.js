12b78b1b3db227e681675e3409bc4aa15d363700
Omar Molina <A01423485@itesm.mx>
2020-11-25 21:21:39 -0600

    alertify

--- Proyecto/Prototipo/Registro_Donadores/ajax.js
+++ Proyecto/Prototipo/Registro_Donadores/ajax.js
@@ -13,6 +13,13 @@ function queryPacientes(){
 }
 */
 function registraDonadores(){
+    
+    if ($("#RazonSocial").val() == '' || $("#RFC").val() == '' || $("#Correo").val() == '' || $("#Telefono").val() == '' || $("#Nacimiento").val() == '' ) {
+        alert('Porfavor llena todos los campos!!');
+    } else {
+        alert('Donador registrado exitosamente!');
+    }
+    
     $.post( "controladorRegistraDonadores.php", 
     {RazonSocial:$("#RazonSocial").val(),RFC:$("#RFC").val(),Correo:$("#Correo").val(),Telefono:$("#Telefono").val(),Nacimiento:$("#Nacimiento").val()})
     .done(function( data ) {
