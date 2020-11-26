d7f089c8a0bc64284269513dc8844af459e84141
AlbertoMatuteB <a01704584@itesm.mx>
2020-11-26 15:48:09 -0600

    Fix funcion asistencia

--- Proyecto/Prototipo/dbUtil.php
+++ Proyecto/Prototipo/dbUtil.php
@@ -1,8 +1,8 @@
 <?php
     function connectDB(){
         $servername="localhost";
-        $username="Matu";
-        $password="Matu$2020";
+        $username="root";
+        $password="";
         $dbname="asuncion1";
 
         $connection=mysqli_connect($servername,$username,$password,$dbname);
