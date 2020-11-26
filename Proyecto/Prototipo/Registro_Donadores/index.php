b5f4a1a17b8d31676a82950638510438b4c46e61
Omar Molina <A01423485@itesm.mx>
2020-11-26 17:08:18 -0600

    prueba

--- Proyecto/Prototipo/Registro_Donadores/index.php
+++ Proyecto/Prototipo/Registro_Donadores/index.php
@@ -1,10 +1,19 @@
 <?php
 
-    require_once 'util.php'; 
+        
+     require_once 'util.php'; 
 
 
-    
-    include("registrarDonadores.html");
+     // if($_SESSION['rol'] == 'Administracion'){
+      
+        include("_header.html");
+        include("../sidebar/index.php");
+        include("registrarDonadores.html");
+        include("_footer.html");
+      //}
+     // else {
+       // echo "ocurrio un error, contacte al administrador";
+      //} 
     
     
 
