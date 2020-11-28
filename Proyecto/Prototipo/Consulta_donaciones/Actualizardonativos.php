038041ef7bbb47382a4b52cd78c9f38f587eff36
Omar Molina <A01423485@itesm.mx>
2020-11-28 12:46:32 -0600

    navbar

--- Proyecto/Prototipo/Consulta_donaciones/Actualizardonativos.php
+++ Proyecto/Prototipo/Consulta_donaciones/Actualizardonativos.php
@@ -1,7 +1,7 @@
 <?php
 
 	include ('_header.html');
-  	include ('_sidebar.html');
+  	include ('../sidebar/_sidebarA.html');
   	include('_form_actualiza_donacion.html');
   	include ('_footer.html');
 
