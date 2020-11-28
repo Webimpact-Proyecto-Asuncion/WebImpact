53624b31b46a26ae96b3d0071e2119426da41e18
Royer <A01209400@itesm.mx>
2020-11-28 18:07:59 +0000

    link

--- Proyecto/Prototipo/Consulta_usuarios/js/main.js
+++ Proyecto/Prototipo/Consulta_usuarios/js/main.js
@@ -47,7 +47,7 @@ function eliminarDatos(id){
 }
 
 function modifyById(id){
-	let auxUrl="../../Modificar_Empleado/ModificaEmpleado.php";
+	let auxUrl="../Modificar_Empleado/ModificaEmpleado.php";
 
 	location.href = auxUrl+"?id="+id;
 }
