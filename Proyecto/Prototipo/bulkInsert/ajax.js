6157b5d7c849fc4c780c82a86b3b4e8e5b180c55
Royer <A01209400@itesm.mx>
2020-11-30 20:26:09 +0000

    upload csv file

---
+++ Proyecto/Prototipo/bulkInsert/ajax.js
@@ -0,0 +1,31 @@
+$("#but_upload").click(function() {
+
+    var fd = new FormData();
+    var files = $('#file')[0].files;
+
+    // Check file selected or not
+    if (files.length > 0) {
+        fd.append('file', files[0]);
+
+        $.ajax({
+            url: 'uploadcsv.php',
+            type: 'post',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                if (response != 0) {
+                    /*$("#img").attr("src", response);
+                    $(".preview img").show(); // Display image element
+                    console.log(response);*/
+                    src = response;
+                    console.log("src: " + src);
+                } else {
+                    alert('file not uploaded');
+                }
+            },
+        });
+    } else {
+        alert("Please select a file.");
+    }
+});
