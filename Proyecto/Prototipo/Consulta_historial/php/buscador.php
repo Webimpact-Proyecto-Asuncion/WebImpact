0f068d1e0521c6f81ed03d1b34275c9a20e3a6a9
SamOctGonzAzpeitia <52299598+SamOctGonzAzpeitia@users.noreply.github.com>
2020-12-01 18:26:43 -0600

    buscador reporte

--- Proyecto/Prototipo/Consulta_historial/php/buscador.php
+++ Proyecto/Prototipo/Consulta_historial/php/buscador.php
@@ -9,9 +9,8 @@
 		if(isset($_POST["consulta"])){
 
 		$q = $conexion->real_escape_string($_POST['consulta']);
-		$sql = "SELECT  R.NumeroReporte, E.NombreEmpleado as 'Encargado', P.NombrePaciente as 'Nombre', P.ApellidoPaterno as 'A Paterno', P.ApellidoMaterno as 'A Materno', R.Valoracion, R.Terapia, R.Tratamiento, R.Avances, R.Fecha
-			FROM Reporte R, Empleado E, Paciente P
-			WHERE E.NumeroEmpleado = R.NumeroEmpleado and P.NumeroPaciente = R.NumeroPaciente and R.Visibilidad = 1 and P.NombrePaciente LIKE '%".$q."%'";
+		$sql = "SELECT  R.NumeroReporte, E.NombreEmpleado as 'Encargado', P.NombrePaciente as 'Nombre', P.ApellidoPaterno as 'A Paterno', P.ApellidoMaterno as 'A Materno', R.Valoracion, R.Terapia, R.Tratamiento, R.Avances, R.Fecha FROM reporte R, Empleado E, Paciente P
+			WHERE E.NumeroEmpleado = R.NumeroEmpleado and P.NumeroPaciente = R.NumeroPaciente and R.Visibilidad = 1 AND P.NombrePaciente LIKE '%".$q."%'";
 
 		$result=mysqli_query($conexion, $sql);
 
