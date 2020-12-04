43c8962614da479560636f82bf618c273721ec31
SamOctGonzAzpeitia <52299598+SamOctGonzAzpeitia@users.noreply.github.com>
2020-12-03 21:54:42 -0600

    sidebar ayuda

--- Proyecto/Prototipo/Ayuda/ayuda.php
+++ Proyecto/Prototipo/Ayuda/ayuda.php
@@ -2,6 +2,7 @@
     session_start();
     
     echo($_SESSION["rol"]);
+
     if($_SESSION["rol"]=="Administracion"){*/
         include ('_head.html');
         include ('../sidebar/_sidebarA.html');
@@ -9,10 +10,10 @@
         include ('_footer.html');
     }else if ($_SESSION["rol"]=="Terapeuta") {
         //cerrar sesion?
-        include_once("./_head.html");
+        include ('_head.html');
         include_once("../sidebar/_sidebarT.html");
-        //include_once("./_dashboardT.html");
-        include_once("./_footer.html");
+        include ('_videos.html');
+        include ('_footer.html');
     } else{
                 include_once("./_head.html");
                 include_once("./_error_rol");
