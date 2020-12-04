081085269c675031eead80c76ba1d35d7336558e
SamOctGonzAzpeitia <52299598+SamOctGonzAzpeitia@users.noreply.github.com>
2020-12-03 18:24:52 -0600

    Se modifico tabla Asistencias

--- Proyecto/Prototipo/Asistencias/php/tabla.php
+++ Proyecto/Prototipo/Asistencias/php/tabla.php
@@ -57,5 +57,58 @@
 		return $tabla;
 	}
 
+	function tabla1 ($page,$consulta){
+
+		$tabla="";
+		$datos="";
+		$result = buscar($consulta);
+		
+		
+
+		while($ver=mysqli_fetch_row($result)){
+
+		$datos=$ver[0]."||".
+			 	$ver[1]."||".
+				$ver[2]."||".
+				$ver[3]."||".
+				$ver[4]."||".
+				$ver[5];
+
+
+
+		$tabla.="<tr>";
+		$tabla.="<td>".$ver[1]."</td>";
+		$tabla.="<td>".$ver[2]."</td>";
+		$tabla.="<td>".$ver[3]."</td>";
+		$tabla.="<td>".$ver[4]."</td>";
+		$tabla.="<td>".$ver[5]."</td>";
+
+
+		$tabla.="<td>
+				<button class='btn btn-success' onclick=confirmaAsistencia('".$ver[0]."')>";
+		$tabla.="<span class='oi oi-check'></span>Asistencia </button>";
+		$tabla.="</td>";
+
+
+		$tabla.="<td>
+				<button class='btn btn-danger' onclick=preguntarSiNo('".$ver[0]."')>";
+		$tabla.="<span class='oi oi-x'></span>Falta </button>";
+		$tabla.="</td>";
+
+
+		$tabla.="<td>
+				<button class='btn btn-primary' onclick=preguntarJustificacion('".$ver[0]."')>";
+		$tabla.="<span class='oi oi-pencil'></span>Justificar </button>";
+		$tabla.="</td>";
+
+		$tabla.="</tr>";
+
+		
+		
+		}
+
+		return $tabla;
+	}
+
 
 ?>
