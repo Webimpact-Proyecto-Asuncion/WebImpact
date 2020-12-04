dc883a10e62c1bb8220334d11531d66c0f01ed26
AlbertoMatuteB <a01704584@itesm.mx>
2020-12-03 19:06:47 -0600

    paginacion Donaciones

--- Proyecto/Prototipo/Consulta_donaciones/php/consulta.php
+++ Proyecto/Prototipo/Consulta_donaciones/php/consulta.php
@@ -2,10 +2,15 @@
 
 	require_once ("conexion.php");
 
-	function consulta(){
+	function consulta($page){
+
+	
 	$conexion=conexion();
 
-	$sql="CALL ImprimirDonaciones";
+	$limit = 10;
+	$start = ($page - 1) * $limit;
+
+	$sql="CALL ImprimirDonaciones($limit, $start)";
 
 	$result=mysqli_query($conexion, $sql);
 
