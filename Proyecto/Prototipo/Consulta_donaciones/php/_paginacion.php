540b654ef8f15fdd5f6c69c4bcc6cf9dd3d90db2
AlbertoMatuteB <a01704584@itesm.mx>
2020-12-03 19:24:56 -0600

    query paginacion

--- Proyecto/Prototipo/Consulta_donaciones/php/_paginacion.php
+++ Proyecto/Prototipo/Consulta_donaciones/php/_paginacion.php
@@ -2,7 +2,7 @@
     include "conexion.php";
     $conexion = conexion();
     $page = isset($_GET['page']) ? $_GET['page'] : 1;	
-    $result1 = $conexion->query("SELECT count(NumeroDonativos) as id FROM Donativos WHERE Visibilidad = 1");
+    $result1 = $conexion->query("SELECT count(NumeroDonativo) as id FROM Donativos WHERE Visibilidad = 1");
 	$custCount = $result1->fetch_all(MYSQLI_ASSOC);
 	$total = $custCount[0]['id'];
 	$pages = ceil( $total / 10 );
