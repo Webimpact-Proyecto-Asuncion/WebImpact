248e2f374a09ea48f9e34c5459cc9855b8a44d7e
AlbertoMatuteB <a01704584@itesm.mx>
2020-12-09 16:22:51 -0600

    fix signin

--- Proyecto/Prototipo/signin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    include_once("./_head.html");
-    //include_once("./_navbar1.html");
-    include_once("./_signinform.html");
-    include_once("../partials/_footer.html");
-?>
